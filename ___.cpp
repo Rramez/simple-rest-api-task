5be7b6567c24bac1de161947968dfec8492929ca
Rramez <150040815+Rramez@users.noreply.github.com>
2024-09-04 14:23:36 +0300

    Add files via upload

---
+++ ___.cpp
@@ -0,0 +1,25 @@
+#ifdef ONLINE_JUDGE
+#define debug(...);
+#else
+#include "dbg/template.cpp"
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define oo 1000000010
+
+void burn(){
+    // يارب يشتغل
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int t=1;
+    cin >> t;
+    while (t--){
+        burn();
+    }
+    return (0-0);
+}
